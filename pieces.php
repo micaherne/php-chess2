59d7a54f90ab25184023c89d6c6477093bcc50ed
Michael Aherne <michael.aherne@strath.ac.uk>
2013-10-18 17:00:04 +0100

    Added moves etc.

--- pieces.php
+++ pieces.php
@@ -24,6 +24,8 @@ class Context {
 		'k' => 'BlackKing'
 	);
 	
+	public $epSquare;
+	
 	public function __construct() {
 		$this->_init();
 	}
@@ -51,6 +53,7 @@ class Context {
 	 * @param $validate throw an Exception if the FEN is invalid?
 	 */
 	public function fromFEN($fenString, $validate=true) {
+		$this->_init();
 		$parts = preg_split('/\s+/', $fenString);
 
 		if ($validate && count($parts) < 4) {
@@ -103,7 +106,9 @@ class Context {
 				continue;
 			}
 			foreach ($rule->moveType->generateMoves($this, $piece) as $move) {
-				$move->result = $rule->result;
+				if (is_null($move->result)) {
+					$move->result = $rule->result;
+				}
 				$result[] = $move;
 			}
 		}
@@ -129,6 +134,92 @@ class Context {
 			}
 		}
 		return $result;
+	}
+	
+	/**
+	 * Make a move and push undo data onto the undo stack. This method is also the main
+	 * way of checking pseudo-legal moves do not leave the moving side in check.
+	 * 
+	 * Note that we do not check the legality of moves in any other way, and assume that
+	 * the move passed to this method is at least pseudo-legal (i.e. would be a legal move
+	 * if it doesn't leave or put the moving side in check). Checking this would be a 
+	 * large performance hit, so we have to assume that calling code is providing a correct
+	 * move.
+	 * 
+	 * @param Move $move
+	 * @return boolean whether the move was made successfully
+	 */
+	public function makeMove(Move $move) {
+		if (is_a($move->result, 'ResultSimple')) {
+			$undo = array('board' => array(
+				$move->from => $this->board[$move->from],
+				$move->to => $this->board[$move->to]
+			));
+			$takenPiece = $this->board[$move->to];
+			$movedPiece = $this->board[$move->from];
+			if (!is_null($takenPiece)) {
+				$takenPiece->location = null;
+			}
+			$this->board[$move->to] = $movedPiece;
+			$movedPiece->location = $move->to;
+			$this->board[$move->from] = null;
+			$this->whiteToMove = !$this->whiteToMove;
+			
+			array_push($this->undo, $undo);
+			
+			if ($this->isCheck($movedPiece->colour)) {
+				$this->undoMove();
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public function undoMove() {
+		$undo = array_pop($this->undo);
+		if (is_null($undo)) {
+			throw new Exception("Attempting to get non-existent undo data");
+		}
+		$this->whiteToMove = !$this->whiteToMove;
+		foreach ($undo['board'] as $square => $piece) {
+			$this->board[$square] = $piece;
+			if (!is_null($piece)) {
+				$piece->location = $square;
+			}
+		}
+		// TODO: Implement
+	}
+	
+	public function isCheck($colour, King $king=null) {
+		if (is_null($king)) {
+			foreach ($this->pieces as $piece) {
+				if (is_null($piece->location)) {
+					continue;
+				}
+				if ($piece->colour == $colour && is_a($piece, 'King')) {
+					$king = $piece;
+					break;
+				}
+			}
+			if (is_null($king)) {
+				throw new Exception("King not found");
+			}
+		}
+		
+		foreach ($this->pieces as $piece) {
+			if (is_null($piece->location)) {
+				continue;
+			}
+			if ($piece->colour == $colour) {
+				continue;
+			}
+			if ($this->attacks($piece, $king->location)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	/**
@@ -231,8 +322,15 @@ class Move {
 	
 	public $from;
 	public $to;
-	
+	public $queeningClass;
 	public $result;
+	
+	public function __construct($from, $to, $queeningClass=null, $result=null) {
+		$this->from = $from;
+		$this->to = $to;
+		$this->queeningClass = $queeningClass;
+		$this->result = $result;
+	}
 	
 }
 
@@ -282,13 +380,7 @@ class Pawn extends Piece {
 	
 	public function __construct() {
 		parent::__construct();
-		$this->rules[] = new Rule(new PrerequisitePawnOnHomeRank(), new MoveTypeSlide(array(new DirectionLinearForward($this)), MoveType::NOCAPTURES, 2), new ResultSimple());
-		$this->rules[] = new Rule(new PrerequisitePawnOnThirdToSixthRank(), new MoveTypeJump(array(new DirectionLinearForward($this)), MoveType::NOCAPTURES), new ResultSimple());
-		$this->rules[] = new Rule(new PrerequisitePawnOnHomeRank(), new MoveTypeJump(array(new DirectionDiagonalForward($this)), MoveType::CAPTURESONLY), new ResultSimple());
-		$this->rules[] = new Rule(new PrerequisitePawnOnThirdToSixthRank(), new MoveTypeJump(array(new DirectionDiagonalForward($this)), MoveType::CAPTURESONLY), new ResultSimple());
-		$this->rules[] = new Rule(new PrerequisitePawnOnQueeningRank(), new MoveTypeJump(array(new DirectionLinearForward($this)), MoveType::NOCAPTURES), new ResultQueening());
-		$this->rules[] = new Rule(new PrerequisitePawnOnQueeningRank(), new MoveTypeJump(array(new DirectionDiagonalForward($this)), MoveType::CAPTURESONLY), new ResultQueening());
-		// TODO: Add e.p.
+		$this->rules[] = new Rule(new PrerequisiteNull(), new MoveTypePawn($this), new ResultSimple());
 	}
 	
 }
@@ -517,24 +609,18 @@ class MoveTypeSlide extends MoveType {
 				for ($i = 0; $i < $this->max; $i++) {
 					$target += $offset;
 					if (!Context::validSquare($target)) {
-						continue 2;
+						break;
 					}
 					$targetPiece = $context->board[$target];
 					if (is_null($targetPiece)) {
 						if ($this->moveType == MoveType::CAPTURESONLY) {
 							continue 2;
 						}
-						$move = new Move();
-						$move->from = $piece->location;
-						$move->to = $target;
-						$result[] = $move;
+						$result[] = new Move($piece->location, $target);
 					} else if ($targetPiece->colour == $piece->colour || $this->moveType == MoveType::NOCAPTURES) {
-						continue 2;
+						break;
 					} else if ($this->moveType == MoveType::MOVEORCAPTURE) {
-						$move = new Move();
-						$move->from = $piece->location;
-						$move->to = $target;
-						$result[] = $move;
+						$result[] = new Move($piece->location, $target);
 					}
 				}
 			}
@@ -553,6 +639,106 @@ class MoveTypeJump extends MoveTypeSlide {
 	
 }
 
+/**
+ * Pawn moves are so unlike piece moves that we give them their own move type. In theory
+ * we could do pawns with multiple rules but it is simpler and better-performing to do a 
+ * custom type.
+ *
+ */
+class MoveTypePawn extends MoveType {
+	
+	private $piece;
+	private $directionMove;
+	private $directionCapture;
+	
+	private static $queeningPieces = array(
+		Piece::WHITE => array(
+			'WhiteKnight',
+			'WhiteBishop',
+			'WhiteRook',
+			'WhiteQueen',
+			'WhiteKing'),
+			
+		Piece::BLACK => array(
+			'BlackKnight',
+			'BlackBishop',
+			'BlackRook',
+			'BlackQueen',
+			'BlackKing')
+	);
+	
+	public function __construct(Piece $piece) {
+		$this->piece = $piece;
+		$this->directionCapture = new DirectionDiagonalForward($piece);
+		$this->directionMove = new DirectionLinearForward($piece);
+	}
+	
+	public function generateMoves(Context $context, Piece $piece) {
+		$result = array();
+		
+		$homeRank = null;
+		$queeningRank = null;
+		if ($piece->colour == Piece::WHITE) {
+			$homeRank = 1;
+			$queeningRank = 7;
+		} else if ($piece->colour == Piece::BLACK) {
+			$homeRank = 6;
+			$queeningRank = 0;
+		}
+		
+		// Move forward one or two
+		$offset = $this->directionMove->offsets[0]; // there is only one
+		$target = $piece->location + $offset;
+		
+		if (is_null($context->board[$target]) && Context::validSquare($target)) {
+			if ($piece->location >> 4 == $queeningRank) {
+				foreach (self::$queeningPieces[$piece->colour] as $className) {
+					$result[] = new Move($piece->location, $target, $className);
+				}
+			} else {
+				$result[] = new Move($piece->location, $target);
+			}
+			if ($piece->location >> 4 == $homeRank) {
+				$target += $offset;
+				if (is_null($context->board[$target]) && Context::validSquare($target)) {
+					$result[] = new Move($piece->location, $target);
+				}
+			}
+		}
+		
+		// Captures
+		foreach ($this->directionCapture->offsets as $offset) {
+			$target = $piece->location;
+
+			$target += $offset;
+			if (Context::validSquare($target)) {
+				$targetPiece = $context->board[$target];
+				if ($context->epSquare == $target) {
+					$result[] = new Move($piece->location, $target, null, new ResultEPCapture());
+				} else if (!is_null($targetPiece) && ($targetPiece->colour != $piece->colour)) {
+					$result[] = new Move($piece->location, $target);
+				}
+			}
+		}
+		
+		return $result;
+	}
+
+	public function generateAttacks(Context $context, Piece $piece) {
+		$result = array();
+		foreach ($this->directionCapture->offsets as $offset) {
+			$target = $piece->location;
+		
+			$target += $offset;
+			if (Context::validSquare($target)) {
+				$result[] = new Move($piece->location, $target);
+			}
+		}
+		
+		return $result;
+	}
+	
+}
 /*
  * Directions
  */
@@ -583,9 +769,9 @@ class DirectionLinearForward extends Direction {
 
 	public function __construct($piece) {
 		if ($piece->colour == Piece::WHITE) {
-			$this->offsets = array(1, 16);
+			$this->offsets = array(16);
 		} else if ($piece->colour == Piece::BLACK) {
-			$this->offsets = array(-16, -1);
+			$this->offsets = array(-16);
 		} else {
 			throw new Exception("Invalid piece colour");
 		}
@@ -672,5 +858,9 @@ class ResultSimple extends Result {
 }
 
 class ResultQueening extends Result {
+	
+}
+
+class ResultEPCapture extends Result {
 	
 }
--- tests/pieces.php
+++ tests/pieces.php
@@ -64,6 +64,12 @@ class PiecesTest extends PHPUnit_Framework_TestCase {
 		$moves = $c->generatePseudoLegalMoves($piece);
 
 		$this->assertCount(7, $moves);
+		
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P1RPP/R2Q2K1 w kq - 0 1');
+		$piece = $c->board[0x51];
+		$this->assertInstanceOf('BlackBishop', $piece);
+		$moves = $c->generatePseudoLegalMoves($piece);
+		$this->assertCount(5, $moves);
 	}
 	
 	public function testGenerateAllPseudoLegalMoves() {
@@ -83,7 +89,70 @@ class PiecesTest extends PHPUnit_Framework_TestCase {
 		$c->startPosition();
 		$piece = $c->board[0x15];
 		$this->assertInstanceOf('WhitePawn', $piece);
-		//$this->assertTrue($c->attacks($piece, 0x26));
-		//$this->assertFalse($c->attacks($piece, 0x67));
+		$this->assertTrue($c->attacks($piece, 0x26));
+		$this->assertFalse($c->attacks($piece, 0x67));
+		
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P1RPP/R2Q2K1 w kq - 0 1');
+		echo $c->display();
+	}
+	
+	public function testMakeMove() {
+		$c = new Context();
+		$c->startPosition();
+		$move = new Move(0x14, 0x34, null, new ResultSimple());
+		$c->makeMove($move);
+		$this->assertCount(1, $c->undo);
+		$this->assertFalse($c->whiteToMove);
+		
+		$move = new Move(0x64, 0x44, null, new ResultSimple());
+		$c->makeMove($move);
+		$this->assertCount(2, $c->undo);
+		$this->assertTrue($c->whiteToMove);
+		
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1');
+		$this->assertTrue($c->isCheck(Piece::WHITE));
+		$move = new Move(0x06, 0x15, null, new ResultSimple());
+		$this->assertFalse($c->makeMove($move));
+		$this->assertTrue($c->whiteToMove);
+		$move = new Move(0x06, 0x07, null, new ResultSimple());
+		$this->assertTrue($c->makeMove($move));
+		$this->assertFalse($c->whiteToMove);
+	}
+	
+	public function testMakeMoveValidation() {
+		$c = new Context();
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1');
+		$i = 0;
+		foreach ($c->generateAllPseudoLegalMoves() as $move) {
+			$result = $c->makeMove($move);
+			if ($result === true) {
+				$i++;
+				$c->undoMove();
+			}
+		}
+		//$this->assertEquals(6, $i);
+	}
+	
+	public function testUndo() {
+		$c = new Context();
+		$c->startPosition();
+		$move = new Move(0x14, 0x34, null, new ResultSimple());
+		$c->makeMove($move);
+		$this->assertCount(1, $c->undo);
+		$c->undoMove();
+		$this->setExpectedException('Exception', 'Attempting to get non-existent undo data');
+		$c->undoMove();
+	}
+	
+	public function testIsCheck() {
+		$c = new Context();
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1');
+		$this->assertTrue($c->isCheck(Piece::WHITE));
+		
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1R1K w kq - 0 1');
+		$this->assertFalse($c->isCheck(Piece::WHITE));
+		
+		$c->fromFEN('r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P1RPP/R2Q2K1 w kq - 0 1');
+		//$this->assertFalse($c->isCheck(Piece::WHITE));
 	}
 }
